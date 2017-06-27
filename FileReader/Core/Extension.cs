70478362eae5e7439a331a6fba9339f48145b0e3
Vikash Rathee <vicky.rathee2005@gmail.com>
2017-06-27 09:03:03 +0530

    Read large file in chunks with limit and offset
    Read large file in chunks with limit and offset to avoid out of memory exceptions

--- FileReader/Core/Extension.cs
+++ FileReader/Core/Extension.cs
@@ -37,5 +37,50 @@ namespace FileReader.Core
             }
             return table;
         }
+
+        public static DataTable FileToTable(this string path, bool heading = true, char delimiter = '\t', int offset = 0, int limit = 100000)
+        {
+            var table = new DataTable();
+            string headerLine = File.ReadLines(path).FirstOrDefault(); // Read the first row for headings
+            string[] headers = headerLine.Split(delimiter);
+            int skip = 1;
+            int num = 1;
+            foreach (string header in headers)
+            {
+                if (heading)
+                {
+                    table.Columns.Add(header); 
+                }
+                else
+                {
+                    table.Columns.Add("Field" + num); // Create fields header if heading is false
+                    num++;
+                    skip = 0; // Don't skip the first row if heading is false
+                }
+            }
+
+            foreach (string line in File.ReadLines(path).Skip(skip + offset).Take(limit))
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    table.Rows.Add(line.Split(delimiter));
+                }
+            }
+            return table;
+        }
+
+        public static void TableToFile(this DataTable table, string path, bool append = true)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (!File.Exists(path) || !append)
+                stringBuilder.AppendLine(string.Join("\t", table.Columns.Cast<DataColumn>().Select(arg => arg.ColumnName)));
+
+            using (StreamWriter sw = new StreamWriter(path, append))
+            {
+                foreach (DataRow dataRow in table.Rows)
+                    stringBuilder.AppendLine(string.Join("\t", dataRow.ItemArray.Select(arg => arg.ToString())));
+                sw.Write(stringBuilder.ToString());
+            }
+        }
     }
 }
--- FileReader/Program.cs
+++ FileReader/Program.cs
@@ -12,16 +12,50 @@ namespace FileReader
     {
         static void Main(string[] args)
         {
+
             string Path = @"C:\sample.txt";
 
+            ReadFullFile(Path); // Read full file
+
+            ReadFileInBatch(Path); // Read 1 or more gb large file in chunks
+
+            Console.Read();
+        }
+
+        /* Example 1 :: Read Full file */
+        internal static void ReadFullFile(string Path)
+        {
             if (File.Exists(Path)) // Check if local path is valid
             {
                 var table = Path.FileToTable(heading: true, delimiter: '\t');
-                    
+
                 // Process your file here
 
-                Console.WriteLine(table.Rows.Count);
-                Console.Read();
+                table.TableToFile(@"C:\output.txt");
+            }
+        }
+
+        /* Example 2 :: Read file in small chunks */
+        internal static void ReadFileInBatch(string Path)
+        {
+            int TotalRows = File.ReadLines(Path).Count(); // Count the number of rows in file with lazy load
+            int Limit = 100000;
+            for (int Offset = 0; Offset < TotalRows; Offset += Limit)
+            {
+                // Print Logs
+                string Logs = string.Format("Processing :: Rows {0} of Total {1} :: Offset {2} : Limit : {3}",
+                    (Offset + Limit) < TotalRows ? Offset + Limit : TotalRows,
+                    TotalRows, Offset, Limit
+                );
+
+                Console.WriteLine(Logs);
+
+                var table = Path.FileToTable(heading: true, delimiter: '\t', offset : Offset, limit: Limit);
+
+                // Do all your processing here and with limit and offset and save to drive in append mode
+                // The append mode will write the output in same file for each processed batch.
+
+                table.TableToFile(@"C:\output.txt");
             }
         }
     }
